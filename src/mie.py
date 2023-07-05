a62149c006ee0f362be1d09e125e447876b4690c
Krishnaprasad <mkrishnaprasadvarma@gmail.com>
2023-07-05 13:39:12 +0530

    inv_kin single working

--- src/mie.py
+++ src/mie.py
@@ -28,6 +28,8 @@ def point_finder(path_type,foci,radius,number_of_pts):
 class legjoints:
     thigh_len=5
     ankle_len=5
+    points=[]
+    angles=[]
     def __init__(self):
         # self.name=leg_number
         self.hip_state_pos=PI/6
@@ -51,6 +53,37 @@ class legjoints:
         elif(self.step_index==2):
             if(self.knee_state_pos>=-PI/6):
                 self.knee_state_pos-=0.05
+    
+    def inv_kin_single(self,point):
+        x=point[0]
+        y=point[1]
+        l1=self.thigh_len
+        l2=self.ankle_len
+        c2=((x**2)+(y**2)-(l1**2)-(l2**2))/(2*l1*l2)
+        if(c2<=1 and c2>=-1):
+            s2=math.sqrt(1-(c2**2))
+            theta2=math.atan2(s2,c2)
+        else:
+            print('Cos value error for knee')
+            return None
+        A=l1+(l2*c2)
+        B=(l2*s2)
+        c1=((A*x)+(B*y))/((A**2)+(B**2))
+        if(c1<=1 and c1>=-1):
+            s1=math.sqrt(1-(c1**2))
+            theta1=math.atan2(s1,c1)
+        else:
+            print('Cos value error for hip')
+            return None
+        if(theta1 != None and theta2 != None):
+            # (self.hip_state_pos,self.knee_state_pos)=(theta1,theta2)
+            return (theta1,theta2)
+# print(point_finder('circle',(1,1),2,5))
+    def inv_kin_list(self):
+        angles=[]
+        for i in range(len(self.points)-1):
+            angles.append(self.inv_kin_single(self.points[i]))
+        self.angles=angles
         
         
     def fd_mv_dwn(self):
--- src/test.py
+++ src/test.py
@@ -1,19 +1,47 @@
 import math
-def point_finder(path_type,foci,radius,number_of_pts):
-    division=math.radians(180)/(number_of_pts-1)
-    point_list=[]
-    if(number_of_pts<2):
-        print('Error Min no of pts is 2')
-    if(path_type.lower()=='circle'): 
-        theta=0
-        point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))       
-        point_list.append((round(point[0],4),round(point[1],4)))
-        while(theta != math.radians(180)):
-            theta+=division
-            point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))       
-            point_list.append((round(point[0],4),round(point[1],4)))
-    else:
-        print('Error give proper path type')
-    return point_list
+PI=3.14
+class legjoints:
+    thigh_len=0.5
+    ankle_len=0.5
+    points=[]
+    angles=[]
+    def __init__(self):
+        # self.name=leg_number
+        self.hip_state_pos=PI/6
+        self.knee_state_pos=-PI/6
+        self.hip_state_vel=0
+        self.knee_state_vel=0
+        self.step_index=0
 
-print(point_finder('circle',(1,1),2,5))
+    def inv_kin_single(self,point):
+        x=point[0]
+        y=point[1]
+        l1=self.thigh_len
+        l2=self.ankle_len
+        c2=((x**2)+(y**2)-(l1**2)-(l2**2))/(2*l1*l2)
+        if(c2<=1 and c2>=-1):
+            s2=math.sqrt(1-(c2**2))
+            theta2=math.atan2(s2,c2)
+        else:
+            print('Cos value error for knee')
+            return None
+        A=l1+(l2*c2)
+        B=(l2*s2)
+        c1=((A*x)+(B*y))/((A**2)+(B**2))
+        if(c1<=1 and c1>=-1):
+            s1=math.sqrt(1-(c1**2))
+            theta1=math.atan2(s1,c1)
+        else:
+            print('Cos value error for hip')
+            return None
+        if(theta1 != None and theta2 != None):
+            # (self.hip_state_pos,self.knee_state_pos)=(theta1,theta2)
+            return (theta1,theta2)
+# print(point_finder('circle',(1,1),2,5))
+    def inv_kin_list(self):
+        angles=[]
+        for i in range(len(points)-1):
+            angles.append(self.inv_kin_single(self.points[i]))
+        self.angles=angles
+joint=legjoints()
+print(joint.inv_kin_single((math.sin(math.radians(45)),0)))
