cca1f559aa39e367bd35a97dd7ec8aa2407368eb
Krishnaprasad <mkrishnaprasadvarma@gmail.com>
2023-07-06 14:31:47 +0530

    forward movement done using class

--- src/joint_pub.py
+++ src/joint_pub.py
@@ -6,70 +6,79 @@ from sensor_msgs.msg import JointState
 import time
 import math
 
+num_of_pt=15
 # a function to give the required number of pts as a list
-#give
+#give theta in degrees for lines. Otherwise not required
 def point_finder(path_type,foci,radius,number_of_pts,theta=None):
     point_list=[]
     if(number_of_pts<2):
         print('Error Min no of pts is 2')
-        return
+        return None
     if(path_type.lower()=='circle'): 
         division=math.radians(180)/(number_of_pts-1)
-        theta=0
+        theta=math.radians(180)
         point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))       
         point_list.append((round(point[0],4),round(point[1],4)))
-        while(theta != math.radians(180)):
-            theta+=division
+        while(theta != math.radians(0)):
+            theta-=division
             point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))       
             point_list.append((round(point[0],4),round(point[1],4)))
     elif(path_type.lower()=='linear'):
         ini_pt=foci
         length=0
-        theta=math.radians(theta)
         if(math.isnan(theta)):
             print('Error give direction of line')
-            return
-        division=radius/number_of_pts   #radius used as total length of line
+            return None
+        theta=math.radians(theta)
+        division=radius/number_of_pts  #radius used as total length of line
+        print(division)
         point_list.append(ini_pt)
-        while(length != radius):
+        for i in range(number_of_pts):
             length+=division
+            length=round(length,4)  #binary calculation error is coming if round not included
+            # print(length)
+            # time.sleep(0.5)
             point=(ini_pt[0]+(length*math.cos(theta)),ini_pt[1]+(length*math.sin(theta)))
             point_list.append((round(point[0],4),round(point[1],4)))
     else:
         print('Error give proper path type')
-        return
+        return None
     return point_list
             
 
 class legjoints:
+    global num_of_pt
     thigh_len=5
     ankle_len=5
     points=[]
     angles=[]
-    def __init__(self):
-        # self.name=leg_number
+    def __init__(self,leg_no=1):
+        self.leg_no=leg_no
         self.hip_state_pos=PI/6
         self.knee_state_pos=-PI/6
         self.hip_state_vel=0
         self.knee_state_vel=0
         self.step_index=0
+        self.position_no=0
     def fd_mv_up(self):
-        if(self.step_index==0):
-            if(self.hip_state_pos>=-PI/3):
-                self.hip_state_pos-=0.05
-            if(self.knee_state_pos<=PI/2):
-                self.knee_state_pos+=0.05
-            elif(self.hip_state_pos<=-PI/3):
-                self.step_index=1
-        elif(self.step_index==1):
-            if(self.hip_state_pos<=PI/6):
-                self.hip_state_pos+=0.05
-            else:
-                self.step_index=2
-        elif(self.step_index==2):
-            if(self.knee_state_pos>=-PI/6):
-                self.knee_state_pos-=0.05
-    
+        #each call moves to next position
+        global joint_states
+        if(self.position_no==0):
+            self.points=point_finder('circle',(0,7.5),2.0,num_of_pt)
+            self.inv_kin_list()
+            print('Points : ',self.points)
+            print('angles : ',self.angles)    
+        if(self.position_no<len(self.angles)):
+            joint_states.name.append(f'rota_{self.leg_no}')
+            joint_states.position.append(self.angles[self.position_no][0])
+            joint_states.name.append(f'knee_{self.leg_no}')
+            joint_states.position.append(self.angles[self.position_no][1])
+            print(joint_states)
+            if(self.position_no<len(self.angles)-1):
+                self.position_no+=1
+        else:
+            return        
+
     def inv_kin_single(self,point):
         x=point[0]
         y=point[1]
@@ -100,34 +109,34 @@ class legjoints:
         for i in range(len(self.points)):
             angles.append(self.inv_kin_single(self.points[i]))
         self.angles=angles
-        
-        
+
+    def fd_kin_single(self,angles):
+        l1=self.thigh_len
+        l2=self.ankle_len
+        x=(l1*math.cos(angles[0]))+(l2*math.cos(angles[0]+angles[1]))
+        y=(l1*math.sin(angles[0]))+(l2*math.sin(angles[0]+angles[1]))
+        return ((round(x,4),round(y,4)))
+
     def fd_mv_dwn(self):
         pass
 
 
 
-leg1=legjoints()
-leg2=legjoints()
-leg3=legjoints()
-leg4=legjoints()
-legs=[leg1,leg2,leg3,leg4]
+leg1=legjoints(1)
+leg2=legjoints(2)
+leg3=legjoints(3)
+leg4=legjoints(4)
+joint_states=JointState()
+
 def talker():
-    global legs,stepindex
+    global leg1,leg2,leg3,leg4,joint_states,num_of_pt
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     rospy.init_node('joint_state_publisher')
     rate = rospy.Rate(2) # 10hz
-    #finding angles
-    legs[0].points=point_finder('circle',(0,7.5),2.0,15)
-    legs[0].inv_kin_list()
-    print('Points : ',leg1.points)
-    print('angles : ',leg1.angles)    
-    index=0
     
-    while not rospy.is_shutdown():
-        # joints.fd_mv_up()
-
-        joint_states=JointState()
+    while not rospy.is_shutdown():     
+        joint_states.name.clear()
+        joint_states.position.clear()
         joint_states.header.stamp=rospy.get_rostime()
 
         # pulishing below this axis trail
@@ -139,26 +148,13 @@ def talker():
         # pub.publish(joint_states)
         # rate.sleep()
 
-        #individual inv_kin trial
-        # point=input()
-        # angle=leg1.inv_kin_single((-3,3.5))
-        # joint_states.name.append('rota')
-        # joint_states.position.append(angle[0])
-        # joint_states.name.append('knee')
-        # joint_states.position.append(-angle[1])
-        # pub.publish(joint_states)
-        # rate.sleep()
-
-        # #full inverse kinematics trial
-        if(index<len(legs[0].angles)):
-            # print(leg1.angles[index][0])
-            for i in range(1,5):
-                legs[i-1]=legs[0]
-                joint_states.name.append(f'rota_{i}')
-                joint_states.position.append(legs[i-1].angles[index][0])
-                joint_states.name.append(f'knee_{i}')
-                joint_states.position.append(legs[i-1].angles[index][1])
-            index+=1
+        i=0
+        if(i<num_of_pt):
+            i+=1
+            leg1.fd_mv_up()
+            # leg2.fd_mv_up()
+            leg3.fd_mv_up()
+            # leg4.fd_mv_up()
             pub.publish(joint_states)
             rate.sleep()
 
