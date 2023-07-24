7ecc4f1694d21775a9203c96a2c1aacc125b6053
Krishnaprasad <mkrishnaprasadvarma@gmail.com>
2023-07-24 10:09:16 +0530

    New matlab model working given joint POS

--- src/test.py
+++ src/test.py
@@ -1,39 +1,56 @@
 import math
 import time
-
-def fd_kin_single(angles,len1,len2):
-        l1=len1
-        l2=len2
-        x=(l1*math.cos(-angles[0]))+(l2*math.cos(-angles[0]+angles[1]))
-        y=(l1*math.sin(-angles[0]))+(l2*math.sin(-angles[0]+angles[1]))
-        return (round(x,4),round(y,4))
-
-
-def inv_kin_single(point,len1,len2):
-        x=point[0]
-        y=point[1]
-        c2=((x**2)+(y**2)-(len1**2)-(len2**2))/(2*len1*len2)
-        # print('c2 ',c2)
-        if(c2<=1 and c2>=-1):
-            theta2=math.acos(c2)
-            # s2=math.sqrt(1-(c2**2))
-            # theta2=math.atan2(s2,c2)
-        else:
-            print(f'Cos value error for c2 on pt ({x},{y})')
+def point_finder(path_type,foci,radius,number_of_pts,theta=None):
+    point_list=[]
+    if(number_of_pts<2):
+        print('Error Min no of pts is 2')
+        return None
+    if(path_type.lower()=='circle'): 
+        division=math.radians(180)/(number_of_pts-1)
+        theta=-math.radians(180)
+        point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))  
+        # print(point)     
+        point_list.append((round(point[0],4),round(point[1],4)))
+        for i in range(number_of_pts-1):
+            theta+=division
+            point=(foci[0]+(radius*math.cos(theta)),foci[1]+(radius*math.sin(theta)))    
+            # print(point)   
+            point_list.append((round(point[0],4),round(point[1],4)))
+    elif(path_type.lower()=='linear'):
+        ini_pt=foci
+        length=0
+        if(math.isnan(theta)):
+            print('Error give direction of line')
             return None
-        A=len1+(len2*c2)
-        B=(len2*math.sin(theta2))
-        c1=((A*x)+(B*y))/((A**2)+(B**2))
-        # print('c1 ',c1)
-        if(c1<=1 and c1>=-1):
-            theta1=-math.acos(c1)   #require value in third or fourth quad. So -ve of ans of arccos. Its ans in 1st or 2nd quad
-            # print('t1 ',theta1*180/3.14)
-        else:
-            print('Cos value error for hip')
-            return None
-        if(theta1 != None and theta2 != None):
-            # (self.hip_state_pos,self.knee_state_pos)=(theta1,theta2)
-            return (theta1,theta2)
-        
-print('inv ans',inv_kin_single((1.5,11.5),10,2))
-print('fwd ans',fd_kin_single((-1.329,0.703),10,2))
+        theta=math.radians(theta)
+        division=radius/number_of_pts  #radius used as total length of line
+        # print(division)
+        point_list.append(ini_pt)
+        for i in range(number_of_pts):
+            length+=division
+            length=round(length,4)  #binary calculation error is coming if round not included
+            # print(length)
+            # time.sleep(0.5)
+            point=(ini_pt[0]+(length*math.cos(theta)),ini_pt[1]+(length*math.sin(theta)))
+            point_list.append((round(point[0],4),round(point[1],4)))
+    else:
+        print('Error give proper path type')
+        return None
+    return point_list
+from elephant import legjoints
+
+
+p=point_finder('circle',(0,11.5),1,10)
+js=legjoints('hind',1)
+js.fd_mv_up()
+# print(js.angles)
+x=[]
+y=[]
+z=[]
+for i in range(50):
+    x.append(js.angles[i][0])
+    y.append(js.angles[i][1])
+    z.append(js.angles[i][2])
+print(x)
+print(y)
+print(z)
